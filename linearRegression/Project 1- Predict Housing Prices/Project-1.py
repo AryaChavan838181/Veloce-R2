813069ba9b9fe26be3724a8c8e3c2b8983c80a0c
Arya Chavan <88090292+AryaChavan838181@users.noreply.github.com>
2025-04-06 23:44:53 +0530

    Intial Setup

---
+++ linearRegression/Project 1- Predict Housing Prices/Project-1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.linear_model import LinearRegression
+
