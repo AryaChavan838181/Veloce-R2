289e2dba1c4ad1227c8c9fcb9bd5c4ae9a90e6b6
Arya Chavan <88090292+AryaChavan838181@users.noreply.github.com>
2025-04-07 00:14:56 +0530

    a

--- linearRegression/Project 1- Predict Housing Prices/Project-1.py
+++ linearRegression/Project 1- Predict Housing Prices/Project-1.py
@@ -1,10 +1,19 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import tensorflow as tf
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
+from scipy.stats import pearsonr
+from sklearn.model_selection import train_test_split as tts
+from sklearn.linear_model import LinearRegression as lr
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.model_selection import cross_val_score as cvs
 
+
+# Load the dataset
+#column_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+dataset = pd.read_csv('linearRegression\\Project 1- Predict Housing Prices\\HousingData.csv', delimiter=',')#, names=column_names)
+print(dataset.head())
