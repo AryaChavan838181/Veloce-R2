1284a6eb45c3206ef08b25f4552531798faa78e7
Arya Chavan <88090292+AryaChavan838181@users.noreply.github.com>
2025-04-07 00:38:53 +0530

    update

--- linearRegression/Project 1- Predict Housing Prices/Project-1.py
+++ linearRegression/Project 1- Predict Housing Prices/Project-1.py
@@ -17,3 +17,7 @@ from sklearn.model_selection import cross_val_score as cvs
 #column_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
 dataset = pd.read_csv('linearRegression\\Project 1- Predict Housing Prices\\HousingData.csv', delimiter=',')#, names=column_names)
 print(dataset.head())
+
+print(dataset.describe())
+dataset = dataset.drop(['ZN', 'CHAS'], axis=1)
+print(dataset.isnull().sum())
