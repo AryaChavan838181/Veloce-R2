af49bf3ca60e75b1b50fd2025daf3e2356be8c5e
Arya Chavan <88090292+AryaChavan838181@users.noreply.github.com>
2025-10-18 00:31:19 +0530

    Delete LogisticRegression directory

--- LogisticRegression/Project 1 Predict Customer Churn/Project 1.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import tensorflow as tf
-from sklearn.model_selection import train_test_split   
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, roc_auc_score, roc_curve
-from sklearn.linear_model import LogisticRegression
